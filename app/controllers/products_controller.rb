801c1c674d9a4446f49d2085ceda675c318b80f2
takessy <takessy@docomo.ne.jp>
2022-08-30 14:48:59 +0900

    04 create products table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,23 +14,29 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.category_id = 1
     @product.save
     redirect_to product_url @product
   end
   
   def edit
     @product = Product.find(params[:id])
+    @Categories = Category.all
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_url @product
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to product_url
   end
 
   private
     def product_params
-      params.require(:product).permit(:name, :description, :price)
+      params.require(:product).permit(:name, :description, :price :Category_id)
     end
 end
