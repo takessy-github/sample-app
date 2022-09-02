47d6027eed502c25f2bcbbb4dd03fab8a3a9c22b
takessy <takessy@docomo.ne.jp>
2022-09-02 12:10:19 +0900

    18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user
+  before_action :authenticate_user!
+
   def edit
   end
 
@@ -14,6 +16,24 @@ class UsersController < ApplicationController
   def edit_address
   end
 
+  def update_password
+    if password_set?
+      @user.update_password(user_params) 
+      flash[:notice] = "パスワードは正しく更新されました。"
+      redirect_to root_url
+    else
+      @user.errors.add(:password, "パスワードに不備があります。")
+      render "edit_password"
+    end
+  end
+
+  def edit_password
+  end
+
+  def favorite
+    @favorites = @user.likees(Product)
+  end
+
   private
     def set_user
       @user = current_user
@@ -21,5 +41,10 @@ class UsersController < ApplicationController
 
     def user_params
       params.permit(:name, :email, :address, :phone, :password, :password_confirmation)
+    end
+
+    def password_set?
+      user_params[:password].present? && user_params[:password_confirmation].present? ?
+      true : false
     end
 end
